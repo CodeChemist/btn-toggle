ab7fa5941dfecc1c8e0a25b3146ec6b38172dd88
CodeChemist <CodeChemist@users.noreply.github.com>
2018-04-27 23:03:27 -0700

    Changed .val() to .attr('value'); Set initial val
    Using .attr('value') allows elements other than buttons and inputs (like tr, td, li, etc.) to be clickable on-off buttons. Sets initial values so elements don't require a value attribute to be manually added to html.

--- btn-toggle.js
+++ btn-toggle.js
@@ -2,7 +2,7 @@
  *	JavaScript for btn-toggle
  *
  *	@author		Chris Finley
- *	@version	1.0.0
+ *	@version	1.0.1
  *	@date		2018-04-27
  *	@requires	jquery 1.7+
  */
@@ -10,11 +10,16 @@
  // requires jquery
 $(document).ready(function()
 {
+	// set initial values, defaults to off unless has .on class
+	$('.btn-toggle').attr({ value: 0 });
+	$('.btn-toggle.on').attr({ value: 1 });
+		
 	// register click event handler
 	$(document).on('click', '.btn-toggle', function()
 	{
 		// parse the current value and use modulus to toggle between 0 and 1
 		// trigger change event to fire an event handler to respond to value change
-		$(this).val( ( parseInt( $(this).val() ) + 1 ) % 2 ).trigger('change');
+		// using .attr(value) because .val() does not work on non-input elements like tr, td, li, etc
+		$(this).attr({ value: ( parseInt( $(this).attr('value') ) + 1 ) % 2 }).trigger('change');
 	});
 });
